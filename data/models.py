3de1e7f6145a0f978c1f2bf425679d439ca092e8
limoiie <limo.iie4@gmail.com>
2017-10-26 19:07:06 +0800

    <docs>: Add docs to two models and functions

--- data/models.py
+++ data/models.py
@@ -4,6 +4,7 @@ import json
 
 
 class RawRecord01(models.Model):
+    """ Model of raw record """
     newsId = models.CharField(max_length=40, primary_key=True)
     srcUrl = models.URLField()
     title = models.CharField(max_length=500)
@@ -12,6 +13,20 @@ class RawRecord01(models.Model):
 
     @staticmethod
     def load_from_json(json_str):
+        """
+        Load raw record from json str. The format of json should be
+        like this:
+        {
+            "newsId": "111111111111111111111",
+            "srcUrl": "http://www.baidu.com",
+            "title": "This is title",
+            "postTime": "1504171080000",
+            "body": "This is body"
+        }
+        each raw record is corresponding to one json string
+        :param json_str: json string with format like above
+        :return: record
+        """
         obj = json.loads(json_str)
         record = RawRecord01(newsId=obj['newsId'],
                              srcUrl=obj['srcUrl'],
@@ -22,6 +37,11 @@ class RawRecord01(models.Model):
 
 
 class Result01(models.Model):
+    """
+    Model of result under the analysis of raw result, each record
+    will have several results, each result is about one entity in
+    record.
+    """
     raw_record = models.ForeignKey(RawRecord01, on_delete=models.CASCADE)
     eventLevel = models.CharField(max_length=20)
     keywords = models.CharField(max_length=400)
@@ -30,6 +50,33 @@ class Result01(models.Model):
 
     @staticmethod
     def load_from_json(json_str):
+        """
+        Load %class Result01 from json. The format of json should
+        be like:
+        {
+            "newsId": "111111111111111111111",
+            "entities": [
+                 {
+                     "eventLevel": "event-value",
+                     "keywords": "keyword1|keyword2",
+                     "name": "name-value",
+                     "digest": "digest-value"
+                 },
+                 {
+                     "eventLevel": "event-value",
+                     "keywords": "keyword1|keyword2",
+                     "name": "name-value",
+                     "digest": "digest-value"
+                 },
+                 ...
+            ]
+        }
+        each result is corresponding to one entity in json string
+        show above with the same newsId.
+
+        :param json_str: json string with format like above
+        :return: yield one result each time.
+        """
         obj = json.loads(json_str)
         record = RawRecord01.objects.get(pk=obj['newsId'])
         assert record is not None
