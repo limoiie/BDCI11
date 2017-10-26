719a2a1aff5eb97fc65e2f63516de15098cd9a78
limoiie <limo.iie4@gmail.com>
2017-10-26 23:07:45 +0800

    <refactor|test>(model): Refactor models
    Refactor model Result01 and model RawRecord01, create a new model group
    for grouping records and results. Also, update the test of two models
    involved.

--- data/models.py
+++ data/models.py
@@ -1,18 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
 import json
 
 
+class Group01(models.Model):
+    group_name = models.CharField(max_length=200)
+    post_time = models.DateTimeField(default=timezone.now)
+    pass
+
+
 class RawRecord01(models.Model):
     """ Model of raw record """
-    newsId = models.CharField(max_length=40, primary_key=True)
-    srcUrl = models.URLField()
+    group = models.ForeignKey(Group01, on_delete=models.CASCADE)
+    news_id = models.CharField(max_length=40)
+    src_url = models.URLField()
     title = models.CharField(max_length=500)
-    postTime = models.BigIntegerField()
+    post_time = models.BigIntegerField()
     body = models.TextField()
 
+    class Meta:
+        unique_together = ("group", "news_id")
+
     @staticmethod
-    def load_from_json(json_str):
+    def load_from_json(group, json_str):
         """
         Load raw record from json str. The format of json should be
         like this:
@@ -24,15 +35,18 @@ class RawRecord01(models.Model):
             "body": "This is body"
         }
         each raw record is corresponding to one json string
+        :param group: instance of %class Group01
         :param json_str: json string with format like above
         :return: record
         """
+        assert isinstance(group, Group01)
+
         obj = json.loads(json_str)
-        record = RawRecord01(newsId=obj['newsId'],
-                             srcUrl=obj['srcUrl'],
-                             title=obj['title'],
-                             postTime=obj['postTime'],
-                             body=obj['body'])
+        record = group.rawrecord01_set.create(news_id=obj['newsId'],
+                                              src_url=obj['srcUrl'],
+                                              title=obj['title'],
+                                              post_time=obj['postTime'],
+                                              body=obj['body'])
         return record
 
 
@@ -43,46 +57,48 @@ class Result01(models.Model):
     record.
     """
     raw_record = models.ForeignKey(RawRecord01, on_delete=models.CASCADE)
-    eventLevel = models.CharField(max_length=20)
+    event_level = models.CharField(max_length=20)
     keywords = models.CharField(max_length=400)
     name = models.CharField(max_length=200)
     digest = models.TextField()
 
     @staticmethod
-    def load_from_json(json_str):
+    def load_from_json(group, json_str):
         """
         Load %class Result01 from json. The format of json should
         be like:
         {
-            "newsId": "111111111111111111111",
+            "newsId": "11111111111111111111",
             "entities": [
-                 {
-                     "eventLevel": "event-value",
-                     "keywords": "keyword1|keyword2",
-                     "name": "name-value",
-                     "digest": "digest-value"
-                 },
-                 {
-                     "eventLevel": "event-value",
-                     "keywords": "keyword1|keyword2",
-                     "name": "name-value",
-                     "digest": "digest-value"
-                 },
-                 ...
+                {
+                    "eventLevel": "event-value",
+                    "keywords": "keyword1|keyword2",
+                    "name": "name-value",
+                    "digest": "digest-value"
+                },
+                {
+                    "eventLevel": "event-value",
+                    "keywords": "keyword1|keyword2",
+                    "name": "name-value",
+                    "digest": "digest-value"
+                }
             ]
         }
-        each result is corresponding to one entity in json string
-        show above with the same newsId.
+        several results are corresponding to one raw record
 
+        :param group: instance of %class Group01
         :param json_str: json string with format like above
-        :return: yield one result each time.
+        :return: result
         """
-        obj = json.loads(json_str)
-        record = RawRecord01.objects.get(pk=obj['newsId'])
-        assert record is not None
-        for entity in obj['entities']:
-            result = record.result01_set.create(eventLevel=entity['eventLevel'],
-                                                keywords=entity['keywords'],
-                                                name=entity['name'],
-                                                digest=entity['digest'])
-            yield result
+        assert isinstance(group, Group01)
+        root = json.loads(json_str)
+        raw_record = RawRecord01.objects.get(group=group, news_id=root['newsId'])
+
+        results = list()
+        for entity in root['entities']:
+            result = raw_record.result01_set.create(event_level=entity['eventLevel'],
+                                                    keywords=entity['keywords'],
+                                                    name=entity['name'],
+                                                    digest=entity['digest'])
+            results.append(result)
+        return results
--- data/tests.py
+++ data/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 import json
 
-from data.models import RawRecord01, Result01
+from data.models import RawRecord01, Result01, Group01
 
 test_records = [
     '{"newsId":"aa8ca5ff0a1b882dff018b1e3353f16e409b33b8",'
@@ -36,14 +36,21 @@ class RawRecord01ModelTests(TestCase):
     def test_load_from_json():
         # extract from json
         dicts = [json.loads(j) for j in test_records]
+
+        # one group for one file of raw records
+        group = Group01.objects.create(group_name='group')
+
+        records = list()
         # extract from tested method
-        records = [RawRecord01.load_from_json(j) for j in test_records]
+        for j in test_records:
+            record = RawRecord01.load_from_json(group, j)
+            records.append(record)
 
         for a, b in zip(dicts, records):
-            assert a['newsId'] == b.newsId
-            assert a['srcUrl'] == b.srcUrl
+            assert a['newsId'] == b.news_id
+            assert a['srcUrl'] == b.src_url
             assert a['title'] == b.title
-            assert a['postTime'] == b.postTime
+            assert a['postTime'] == b.post_time
             assert a['body'] == b.body
 
         print('\n=> RawRecord01ModelTests.test_load_from_json pass!')
@@ -51,36 +58,53 @@ class RawRecord01ModelTests(TestCase):
 
 class Result01ModelTests(TestCase):
     @staticmethod
+    def extract_results_from_json():
+        uniform_results = dict()
+        for j in test_results:
+            result = json.loads(j)
+            uniform_results[result['newsId']] = result['entities']
+        return uniform_results
+
+    @staticmethod
+    def extract_results_by_method(group):
+        """ Extract results by %code Result01.load_from_json """
+
+        results = list()
+        # extract results from tested method
+        for j in test_results:
+            results_per_record = Result01.load_from_json(group, j)
+            results.extend(results_per_record)
+
+        results_uniform = dict()
+        # construct uniform records for compare conveniently
+        for result in results:
+            d = dict()
+            d['eventLevel'] = result.event_level
+            d['keywords'] = result.keywords
+            d['name'] = result.name
+            d['digest'] = result.digest
+
+            news_id = result.raw_record.news_id
+            if news_id not in results_uniform.keys():
+                results_uniform[news_id] = list()
+            results_uniform[news_id].append(d)
+        return results_uniform
+
+    @staticmethod
     def test_load_from_json():
+        # one group for one file of raw records
+        group = Group01.objects.create(group_name='group')
+
         # extract records
-        records = [RawRecord01.load_from_json(j) for j in test_records]
+        records = [RawRecord01.load_from_json(group, j) for j in test_records]
         # save record into database from which %code Result01.load_from_json will read
         for record in records:
             record.save()
 
-        # extract results from json
-        results_uniform_from_json = dict()
-        for j in test_results:
-            result = json.loads(j)
-            results_uniform_from_json[result['newsId']] = result['entities']
+        # extract results by two different method
+        results_from_json = Result01ModelTests.extract_results_from_json()
+        results_by_method = Result01ModelTests.extract_results_by_method(group)
 
-        results_uniform = dict()
-        # extract results from tested method
-        results = [Result01.load_from_json(j) for j in test_results]
-        # construct uniform records for compare conveniently
-        for results_per_record in results:
-            news_id = None
-            ls = list()
-            for result in results_per_record:
-                news_id = result.raw_record_id if news_id is None else news_id
-                d = dict()
-                d['eventLevel'] = result.eventLevel
-                d['keywords'] = result.keywords
-                d['name'] = result.name
-                d['digest'] = result.digest
-                ls.append(d)
-            results_uniform[news_id] = ls
-
-        assert results_uniform_from_json == results_uniform
+        assert results_from_json == results_by_method
 
         print('\n=> Result01ModelTests.test_load_from_json pass!')
