1153d23922e2337d38ba9eabce00d2ab34c296b6
limoiie <limo.iie4@gmail.com>
2017-10-30 23:31:13 +0800

    <refactor>: Remove html pages
    Remove the html pages, now we will update data through cl-tools

--- data/admin.py
+++ data/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from data.models import *
 
+
+class ResultInline(admin.TabularInline):
+    model = Result01
+    extra = 0
+
+
+class RecordAdmin(admin.ModelAdmin):
+    inlines = [ResultInline]
+    list_display = ('title', 'post_time')
+
+
 admin.site.register(Group01)
-admin.site.register(RawRecord01)
+admin.site.register(RawRecord01, RecordAdmin)
 admin.site.register(Result01)
--- data/controllers.py
+++ data/controllers.py
@@ -3,25 +3,27 @@ from json import JSONDecodeError
 from data.models import *
 
 
-def extract_records_and_store(file, name):
-    group = Group01.objects.create(group_name=name)
-    try:
+def extract_records_and_store(file_name, name):
+    file = open(file_name, encoding='utf-8')
+    if file and not file.closed:
+        group = Group01.objects.create(group_name=name)
+
+        num = 0
         for line in file:
+            print('process line %d ...' % num)
+            num += 1
             RawRecord01.load_from_json(group, line)
-    except JSONDecodeError:
-        group.delete()
-        return False
-    return True
-
-
-def extract_results_and_store(file, group_id):
-    group = Group01.objects.get(pk=group_id)
-    if group:
-        try:
-            for line in file:
-                Result01.load_from_json(group, line)
-        except JSONDecodeError:
-            return False
     else:
-        return False
-    return True
+        raise Exception('Fall to open file %s.' % file_name)
+
+
+def extract_results_and_store(file_name, group):
+    file = open(file_name, encoding='utf-8')
+    if file and not file.closed:
+        num = 0
+        for line in file:
+            print('process line %d ...' % num)
+            num += 1
+            Result01.load_from_json(group, line)
+    else:
+        raise Exception('Fall to open file %s.' % file_name)
